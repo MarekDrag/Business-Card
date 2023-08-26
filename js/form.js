5ddd465e6f26e70e31cbdb68dcda7b2f2295685d
Marek <m.drag@neurosys.com>
2023-08-27 01:13:14 +0200

    change email sender provider

--- js/form.js
+++ js/form.js
@@ -1,4 +1,4 @@
-import email from "./email.js";
+// import email from "./email.js";
 
-const form = document.querySelector("#form");
-form.action = `https://formsubmit.io/send/${email}`;
+// const form = document.querySelector("#form");
+// form.action = `https://formsubmit.io/send/${email}`;
